9c86e421834b403f7f5c9bdedfddf448b069e4f3
Larry Harmon <harmonwebdesign@gmail.com>
2024-01-25 01:06:43 -0500

    new infinite tilt gallery

---
+++ tiltGallery/index.js
@@ -0,0 +1,26 @@
+// make sure the gallery isn't too wide for amount of images
+document.addEventListener("DOMContentLoaded", function () {
+  //count the number of images in the gallery
+  var count = document.getElementsByClassName("tilt-gallery-item").length;
+  // tilt-gallery-container max width is count /2 * 200px
+
+  //set the width of the container
+  var container = document.getElementsByClassName("tilt-gallery-container")[0];
+  container.style.maxWidth = (count / 2) * 200 + "px";
+
+  // setup some common lightbox options
+  lightbox.option({
+    resizeDuration: 300,
+    wrapAround: true,
+    fadeDuration: 200,
+    imageFadeDuration: 200,
+    positionFromTop: 100,
+    disableScrolling: true,
+    alwaysShowNavOnTouchDevices: true,
+    disableScrolling: true,
+    fitImagesInViewport: true,
+    maxWidth: 1000,
+    maxHeight: 1000,
+    showImageNumberLabel: false,
+  });
+});
